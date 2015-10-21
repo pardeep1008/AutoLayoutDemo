7136a3b62f957da7885f644ff46384137643052a
pardeep1008 <pardeep1008@gmail.com>
2015-10-21 09:13:00 +0530

    Initial Commit

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Pardeep Kumar on 21/10/2015.
+//  Copyright © 2015 Debut Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
