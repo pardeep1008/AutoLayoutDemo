0d57b2ed6b232b2d287e96af6fd168bf580e8da4
pardeep1008 <pardeep1008@gmail.com>
2015-10-21 10:17:41 +0530

    21-10-2015
    Add new swift file of Second module.

---
+++ AutoLayoutDemo/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Pardeep Kumar on 21/10/2015.
+//  Copyright © 2015 Debut Inc. All rights reserved.
+//
+
+import Foundation
